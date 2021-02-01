40e88c34ffa59b34a4c38a56f5f55253b1c2c086
Eduardo Torres <etorresbar@gmail.com>
2021-01-31 23:16:15 -0700

    improving the list of books

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -1,33 +1,35 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 
+//CSS
+import './index.css'
+const books = [
+  {
+    img:
+      'https://images-na.ssl-images-amazon.com/images/I/51Tlm0GZTXL._SX329_BO1,204,203,200_.jpg',
+    title: 'Atomic Habits',
+    author: 'Amelia Hepworth',
+  },
+  {
+    img:
+      'https://images-na.ssl-images-amazon.com/images/I/41rttRgMMXL._SX326_BO1,204,203,200_.jpg',
+    title: 'Aprender a amar (Español) Pasta blanda',
+    author: 'OSHO',
+  },
+]
 function BookList() {
-  return (
-    <section>
-      <Book />
-    </section>
-  )
+  return <section className='booklist'></section>
 }
 
-const Book = () => {
+const Book = (props) => {
+  // const {img, title, author} = props
   return (
-    <article>
-      <Image></Image>
-      <Title></Title>
-      <Author />
+    <article className='book'>
+      <img src={props.img} alt='' />
+      <h1>{props.title}</h1>
+      <h4>{props.author}</h4>
     </article>
   )
 }
-
-const Image = () => (
-  <img
-    src='https://images-na.ssl-images-amazon.com/images/I/51Tlm0GZTXL._SX329_BO1,204,203,200_.jpg'
-    alt='Atomic Habits'
-  />
-)
-
-const Title = () => <h1>Atomic Habits</h1>
-
-const Author = () => <h4>James Clear</h4>
 
 ReactDom.render(<BookList />, document.getElementById('root'))
