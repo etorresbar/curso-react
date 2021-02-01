7ec02ed24238626f48d631fcfd9d0b555cd211e1
Eduardo Torres <etorresbar@gmail.com>
2021-02-01 00:15:52 -0700

    event handler in react

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -3,31 +3,62 @@ import ReactDom from 'react-dom'
 
 //CSS
 import './index.css'
+
 const books = [
   {
+    id: 1,
     img:
       'https://images-na.ssl-images-amazon.com/images/I/51Tlm0GZTXL._SX329_BO1,204,203,200_.jpg',
     title: 'Atomic Habits',
     author: 'Amelia Hepworth',
   },
   {
+    id: 2,
     img:
       'https://images-na.ssl-images-amazon.com/images/I/41rttRgMMXL._SX326_BO1,204,203,200_.jpg',
     title: 'Aprender a amar (Español) Pasta blanda',
     author: 'OSHO',
   },
 ]
+
 function BookList() {
-  return <section className='booklist'></section>
+  return (
+    <section className='booklist'>
+      {books.map((book) => {
+        return <Book key={book.id} {...book}></Book>
+      })}
+    </section>
+  )
 }
 
-const Book = (props) => {
-  // const {img, title, author} = props
+const Book = ({ img, title, author }) => {
+  // attribute, eventHandler
+  // onclick, onMouseHover
+
+  const clickHandler = () => {
+    alert('Hello world')
+  }
+
+  const complexExample = (author) => {
+    console.log(author)
+  }
+
   return (
-    <article className='book'>
-      <img src={props.img} alt='' />
-      <h1>{props.title}</h1>
-      <h4>{props.author}</h4>
+    <article
+      className='book'
+      onMouseOver={() => {
+        console.log(title)
+      }}
+    >
+      <img src={img} alt='' />
+      <h1 onClick={() => console.log(title)}>{title}</h1>
+      <h4>{author}</h4>
+      <button type='button' onClick={clickHandler}>
+        reference example
+      </button>
+      <button type='button' onClick={() => complexExample(author)}>
+        more complex example
+      </button>
     </article>
   )
 }
